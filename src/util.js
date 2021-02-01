877fbd2cb26edabe7435230462ffb6e024f4f816
Eric Hicks <ehicks05@yahoo.com>
2021-02-01 18:16:57 -0500

    simplify formatRate

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 // UTILITY
 export function myRound(value, places) {
   const multiplier = Math.pow(10, places);
-  return (Math.round(value * multiplier) / multiplier);
+  return Math.round(value * multiplier) / multiplier;
 }
 
 export function camelToTitle(value) {
@@ -10,32 +10,6 @@ export function camelToTitle(value) {
 }
 
 export function formatRate(rate) {
-  let e = 0;
-  let sign = rate >= 0 ? '+' : '-';
-  rate = Math.abs(rate);
-
-  while (rate !== 0 && !(rate >= .01 && rate <= 10)) {
-    if (rate < 1) {
-      rate *= 10;
-      e--;
-    }
-    if (rate > 10) {
-      rate /= 10;
-      e++;
-    }
-  }
-  if (sign === '-')
-    rate *= -1;
-  rate = myRound(rate, 2);
-  if (rate > 0) rate = '+' + rate;
-
-  let rateString = rate.toString();
-  if (rateString.indexOf(".") < 0)
-    rateString += '.';
-
-  while (rateString.indexOf('.') + 3 > rateString.length)
-    rateString += '0';
-
-  if (e !== 0) rateString += 'e' + e;
-  return rateString;
+  let sign = rate > 0 ? "+" : "";
+  return sign + Intl.NumberFormat().format(rate);
 }
