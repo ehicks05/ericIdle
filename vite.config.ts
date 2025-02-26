05b0b083f4c474de4fbdefa3dd148c7f71764d1a
Eric Hicks <ehicks05@yahoo.com>
2025-02-26 11:09:31 -0500

    prep for shad

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
